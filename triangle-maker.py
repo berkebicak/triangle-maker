26557487b2b390092c3c01fcb52bc07e039a250c
Berke Bıçak <berke.bicak@hotmail.com>
2022-10-04 00:06:17 +0300

    triangle maker

---
+++ triangle-maker.py
@@ -0,0 +1,31 @@
+import colorsys
+import turtle
+
+t= turtle.Turtle()
+t.screen.bgcolor("black")
+
+t.penup()
+t.goto(-200,-100)
+t.pendown()
+t.speed(10)
+
+a=400
+h=0.2
+n=50
+
+def tri():
+    global a,n,h
+    for i in range(40):
+        c = colorsys.hsv_to_rgb(h,0.5,0.4)
+        h= h+(1/n)
+        t.color(c)
+        t.forward(a)
+        t.left(120)
+        a= a-10
+for i in range(2):        
+    tri()
+
+
+t.hideturtle()
+turtle.done()
+
